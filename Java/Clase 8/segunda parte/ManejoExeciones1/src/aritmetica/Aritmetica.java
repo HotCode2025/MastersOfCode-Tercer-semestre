77752401f1da7c5ac0c7b2c7b0409366895bee5c
Rafael Wayer Pacheco <rafarafiti19@gmail.com>
2026-06-02 00:21:11 -0300

    Clase 8 subida

---
+++ Java/Clase 8/segunda parte/ManejoExeciones1/src/aritmetica/Aritmetica.java
@@ -0,0 +1,13 @@
+
+package aritmetica;
+
+import excepciones.OperacionesExcepcion;
+
+public class Aritmetica {
+    public static int division(int numerador, int denominador){
+        if(denominador == 0){
+            throw new OperacionesExcepcion("División entre cero");
+        }
+        return numerador / denominador;
+    }        
+}
---
+++ Java/Clase 8/segunda parte/ManejoExeciones1/src/excepciones/OperacionesExcepcion.java
@@ -0,0 +1,9 @@
+
+package excepciones;
+
+public class OperacionesExcepcion extends RuntimeException{
+    //constructor
+    public OperacionesExcepcion(String mensaje){
+        super(mensaje);
+    }
+}
